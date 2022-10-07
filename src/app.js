62bd06b87a432065d47bc1968b928e2d15712808
Jesus David Fuentes <elpadrinojesus@hotmail.com>
2022-10-06 23:16:46 -0500

    mal

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
-const myconnection = require('express-myconnection');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
-const tasksRoutes = require('./routes/tasks');
+asddasd
+const app = express();
+app.set('port', 4000);
+asdasd
+asdasd
 
 const app = express();
 app.set('port', 4000);
